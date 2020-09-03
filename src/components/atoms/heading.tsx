b1e5bfd8a9631b0f18d864c9a2507c750cf7a178
Paul Negrutiu <paul.negrutiu@nearform.com>
2020-09-03 09:00:38 +0300

    onboarding a11y

--- src/components/atoms/heading.tsx
+++ src/components/atoms/heading.tsx
@@ -1,14 +1,9 @@
-import React, {useEffect, useRef} from 'react';
-import {
-  Text,
-  StyleSheet,
-  View,
-  findNodeHandle,
-  AccessibilityInfo
-} from 'react-native';
-import {useFocusEffect, useIsFocused} from '@react-navigation/native';
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
 
-import {Spacing} from './spacing';
+import {useFocusRef} from 'hooks/accessibility';
+
+import {Spacing} from 'components/atoms/layout';
 
 import {text as textStyles, colors} from 'theme';
 
@@ -25,35 +20,11 @@ export const Heading: React.FC<HeadingProps> = ({
   accessibilityFocus = false,
   accessibilityRefocus = false
 }) => {
-  const ref = useRef<any>();
-  const isFocused = useIsFocused();
-  useEffect(() => {
-    if (ref.current && accessibilityFocus) {
-      const tag = findNodeHandle(ref.current);
-      if (tag) {
-        setTimeout(
-          () => ref.current && AccessibilityInfo.setAccessibilityFocus(tag),
-          200
-        );
-      }
-    }
-  }, []);
-
-  useFocusEffect(() => {
-    if (isFocused && accessibilityRefocus && ref.current) {
-      const tag = findNodeHandle(ref.current);
-      if (tag) {
-        setTimeout(
-          () => ref.current && AccessibilityInfo.setAccessibilityFocus(tag),
-          200
-        );
-      }
-    }
-  });
+  const [ref] = useFocusRef({accessibilityFocus, accessibilityRefocus});
 
   return (
     <>
-      <Text importantForAccessibility="yes" ref={ref} style={styles.heading}>
+      <Text accessibilityRole="button" ref={ref} style={styles.heading}>
         {text}
       </Text>
       <View style={[styles.line, !!lineWidth && {width: lineWidth}]}>
