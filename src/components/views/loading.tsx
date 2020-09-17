816cb57997310b00fe0fc60ce0909384e329fe33
John Gill <johnagill95@gmail.com>
2020-09-17 11:54:56 +0100

    replaced icon with png file

--- src/components/views/loading.tsx
+++ src/components/views/loading.tsx
@@ -1,23 +1,29 @@
 import React, {FC} from 'react';
-import {StyleSheet, View, Platform} from 'react-native';
+import {StyleSheet, View, Platform, Image} from 'react-native';
 import {useSafeArea} from 'react-native-safe-area-context';
-
 import {colors} from 'theme';
 import Icons from 'assets/icons';
 
+const HealthLogo = require('assets/images/logo/logo-health-department.png');
+
 export const Loading: FC = () => {
   const insets = useSafeArea();
-  const logoStateHeight = Platform.OS === 'ios' ? 240 : 335;
+
   return (
     <View style={[styles.container, {paddingBottom: insets.bottom}]}>
       <View style={styles.appLogo}>
         <Icons.LogoLaunch width={242} height={242} color={colors.white} />
       </View>
       <View style={styles.stateLogo}>
-        <Icons.LogoState
+        {/*<Icons.LogoState
           width={logoStateHeight}
           height={80}
           color={colors.white}
+        />*/}
+        <Image
+          accessibilityIgnoresInvertColors
+          style={styles.stateLogoImage}
+          source={HealthLogo}
         />
       </View>
     </View>
@@ -38,9 +44,13 @@ const styles = StyleSheet.create({
   },
   stateLogo: {
     position: 'absolute',
-    bottom: 40,
+    bottom: 55,
     left: 0,
     right: 0,
     alignItems: 'center'
+  },
+  stateLogoImage: {
+    height: 50,
+    width: 240
   }
 });
