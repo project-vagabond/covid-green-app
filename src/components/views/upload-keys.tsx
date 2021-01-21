0b05b7fc4ffa74e6522ea3d12fdce5f71684c3d6
AlanSl <alan.slater@nearform.com>
2021-01-21 16:18:58 +0000

    Never focus disabled button

--- src/components/views/upload-keys.tsx
+++ src/components/views/upload-keys.tsx
@@ -235,7 +235,7 @@ export const UploadKeys: FC<{
       !validationError
     );
 
-    const onDoneHandler = () => setAccessibilityFocusRef(okayRef);
+    const onDoneHandler = () => !okayDisabled && setAccessibilityFocusRef(okayRef);
 
     return (
       <View key={inputKey}>
